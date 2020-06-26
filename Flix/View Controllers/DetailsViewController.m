4fc32312e050bf1554167ce7f60adcb92bfa7258
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-06-26 18:05:24 -0500

    Added check for missing backdrop, when its missing it pulls the poster image instead

--- Flix/View Controllers/DetailsViewController.m
+++ Flix/View Controllers/DetailsViewController.m
@@ -21,7 +21,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // Construct the string for the poster and backdrop
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
     NSString *posterURLString = self.movie[@"poster_path"];
     
@@ -30,18 +30,21 @@
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
     [self.posterView setImageWithURL:posterURL];
     
-    
-    
-    
-    
-    NSString *backdropURLString = self.movie[@"backdrop_path"];
+
+    // Check if the movie dictionary was a backdrop path, if not, use the poster path
+    NSString *backdropURLString = nil;
+    if ([self.movie[@"backdrop_path"]  isKindOfClass:[NSNull class]]) {
+        backdropURLString = self.movie[@"poster_path"];
+    } else {
+        backdropURLString = self.movie[@"backdrop_path"];
+    }
     
     NSString *fullBackdropURLString = [baseURLString stringByAppendingString:backdropURLString];
     
     NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
     [self.backdropView setImageWithURL:backdropURL];
     
-    
+    // Set the title and synopsis
     self.titleLabel.text = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"overview"];
     
